127f565a22ea7ca583a0fb2adc7a529af329fc9e
Milena Grncarova <milenagrncarov@outlook.com>
2021-03-02 14:44:12 +0100

    Fetching a json

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
 
   constructor(){
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
 
     };
   }
 
+  componentDidMount(){
+    fetch ('https://jsonplaceholder.typicode.com/users').then(response =>
+      response.json())
+      .then(users => this.setState({ monsters: users}));
+  }
 
   render () {
     return (
       <div className="App">
         {
-          this.state.monsters.map(monster => <h1>{ monster.name }</h1>)
-        }
+          this.state.monsters.map(monster =>( 
+          <h1 key={monster.id}>{ monster.name }</h1>
+          ))}
       </div>
     );
   }
