5c638784849e2908789b11f8d95037cd0e5dcdd0
Milena Grncarova <milenagrncarov@outlook.com>
2021-03-03 15:40:51 +0100

    Styled the search bar and made it a separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CardList } from './components/card-list/card-list.component'
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends React.Component {
 
@@ -27,7 +28,7 @@ class App extends React.Component {
       );
     return (
       <div className="App">
-      <input type='search' placeholder='Search Monsters' onChange={e => this.setState({ searchField: e.target.value})} />
+      <SearchBox placeholder='Search Monsters' handleChange={e => this.setState({searchField: e.target.value})}/>
       <CardList monsters = {filteredMonsters}></CardList>
       </div>
     );
